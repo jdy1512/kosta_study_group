33abb720575ce40e183ae85108bdf672017e0d40
jdy1512 <dyoung1512@gmail.com>
2015-04-07 20:00:33 +0900

    movieDAO fix

--- [Project1] MovieReservation/MovieReservation_Web/src/kr/co/kosta/study/moviereservation/dao/MovieDAO.java
+++ [Project1] MovieReservation/MovieReservation_Web/src/kr/co/kosta/study/moviereservation/dao/MovieDAO.java
@@ -26,22 +26,38 @@ public class MovieDAO {
 
 	public MovieDAO() {
 		util = UtilManager.getInstance();
+		
+//		final String sql = "insert into movie(movie_id,genre,name,director,actor,start_date,watch_grade_id,running_time,synopsis,imagePath) values("
+//				+ "decode(count(movie_id),0,M1,concat(M,max(substr(movie_id,2))+1)),"
+//				+ "액션/모험/판타지/SF,"
+//				+ "어벤져스: 에이지 오브 울트론,"
+//				+ "조스웨던,"
+//				+ "로버트 다우니 주니어(토니 스타크/아이언맨)^크리스 헴스워스^스칼렛 요한슨,"
+//				+ "20150423,"
+//				+ "select watch_grade_id from watch_grade where watch_grade_name=15세이상관람가,"
+//				+ "141분,"
+//				+ "어벤져스는 끝났다! 희망은 없다! 어벤져스를 위협하는 최강의 적 ‘울트론’의 등장 평화로 가는 길은 단 하나, 인류의 멸종이라고 믿는 ‘울트론’과 사상 최대의 전쟁이 시작된다!,"
+//				+ "C:\\Users\\KOSTA_03_001_\\Desktop\\kosta_study_group\\[Project1] MovieReservation\\movie image\\movie0001.png)";
 	}
 
 	public int insertMovieByAdministrator(Movie movie) throws SQLException {
-		final String sql = "insert into movie values("
-				+ "decode(count(movie_id),0,M1,concat(M,max(substr(movie_id,2))+1))"
-				+ ",?,?,?,?,?,?,?)";
+		final String sql = "insert into movie(movie_id,genre_id,name,director,actor,start_date,watch_grade_id,running_time,synopsis,imagePath) values("
+				+ "decode(count(movie_id),0,M1,concat(M,max(substr(movie_id,2))+1)),"
+				+ "?,?,?,?,?,"
+				+ "select watch_grade_id from watch_grade where watch_grade_name=?,"
+				+ "?,?,?)";
 		Connection conn = util.getConnection();
 		
 		PreparedStatement pstmt = conn.prepareStatement(sql);
-		pstmt.setString(1, movie.getName());
-		pstmt.setString(2, movie.getDirector());
-		pstmt.setString(3, movie.getActor());
-		pstmt.setDate(4, (java.sql.Date) movie.getPlayDate());
-		pstmt.setString(5, movie.getViewingTime());
-		pstmt.setString(6, movie.getSynopsis());
-		pstmt.setString(7, movie.getImagePath());
+		pstmt.setString(1, movie.getGenre());
+		pstmt.setString(2, movie.getName());
+		pstmt.setString(3, movie.getDirector());
+		pstmt.setString(4, movie.getActor());
+		pstmt.setString(5, movie.getPlayDate());
+		pstmt.setString(6, movie.getWatchGrade());
+		pstmt.setString(7, movie.getViewingTime());
+		pstmt.setString(8, movie.getSynopsis());
+		pstmt.setString(9, movie.getImagePath());
 		
 		int result = pstmt.executeUpdate();
 		
--- [Project1] MovieReservation/MovieReservation_Web/src/kr/co/kosta/study/moviereservation/vo/Movie.java
+++ [Project1] MovieReservation/MovieReservation_Web/src/kr/co/kosta/study/moviereservation/vo/Movie.java
@@ -1,42 +1,49 @@
 package kr.co.kosta.study.moviereservation.vo;
 
-import java.util.Date;
 
 /**
  * @author jdy 2015-03-30(최종수정)
  *
- * '영화' VO 클래스'
+ *         '영화' VO 클래스'
  * 
- * name : 영화명
- * imagePath : 영화이미지 경로
- * director : 영화 감독
- * actor : 영화 배우
- * synopsis : 줄거리
- * playDate : 개봉일
- * viewingTime : 상영시간
+ *         name : 영화명 imagePath : 영화이미지 경로 director : 영화 감독 actor : 영화 배우
+ *         synopsis : 줄거리 playDate : 개봉일 viewingTime : 상영시간
  */
 public class Movie {
+	private String genre;
 	private String name;
-	private String imagePath;
 	private String director;
 	private String actor;
-	private String synopsis;
-	private Date playDate;
+	private String playDate;
+	private String watchGrade;
 	private String viewingTime;
+	private String synopsis;
+	private String imagePath;
 
 	public Movie() {
 	}
 
-	public Movie(String name, String imagePath, String director, String actor,
-			String synopsis, Date playDate, String viewingTime) {
+	public Movie(String genre, String name, String director, String actor,
+			String playDate, String watchGrade, String viewingTime,
+			String synopsis, String imagePath) {
 		super();
+		this.genre = genre;
 		this.name = name;
-		this.imagePath = imagePath;
 		this.director = director;
 		this.actor = actor;
-		this.synopsis = synopsis;
 		this.playDate = playDate;
+		this.watchGrade = watchGrade;
 		this.viewingTime = viewingTime;
+		this.synopsis = synopsis;
+		this.imagePath = imagePath;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
 	}
 
 	public String getName() {
@@ -45,14 +52,6 @@ public class Movie {
 
 	public void setName(String name) {
 		this.name = name;
-	}
-
-	public String getImagePath() {
-		return imagePath;
-	}
-
-	public void setImagePath(String imagePath) {
-		this.imagePath = imagePath;
 	}
 
 	public String getDirector() {
@@ -71,20 +70,20 @@ public class Movie {
 		this.actor = actor;
 	}
 
-	public String getSynopsis() {
-		return synopsis;
-	}
-
-	public void setSynopsis(String synopsis) {
-		this.synopsis = synopsis;
-	}
-
-	public Date getPlayDate() {
+	public String getPlayDate() {
 		return playDate;
 	}
 
-	public void setPlayDate(Date playDate) {
+	public void setPlayDate(String playDate) {
 		this.playDate = playDate;
+	}
+
+	public String getWatchGrade() {
+		return watchGrade;
+	}
+
+	public void setWatchGrade(String watchGrade) {
+		this.watchGrade = watchGrade;
 	}
 
 	public String getViewingTime() {
@@ -95,78 +94,28 @@ public class Movie {
 		this.viewingTime = viewingTime;
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((actor == null) ? 0 : actor.hashCode());
-		result = prime * result
-				+ ((director == null) ? 0 : director.hashCode());
-		result = prime * result
-				+ ((imagePath == null) ? 0 : imagePath.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result
-				+ ((playDate == null) ? 0 : playDate.hashCode());
-		result = prime * result
-				+ ((synopsis == null) ? 0 : synopsis.hashCode());
-		result = prime * result
-				+ ((viewingTime == null) ? 0 : viewingTime.hashCode());
-		return result;
+	public String getSynopsis() {
+		return synopsis;
 	}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Movie other = (Movie) obj;
-		if (actor == null) {
-			if (other.actor != null)
-				return false;
-		} else if (!actor.equals(other.actor))
-			return false;
-		if (director == null) {
-			if (other.director != null)
-				return false;
-		} else if (!director.equals(other.director))
-			return false;
-		if (imagePath == null) {
-			if (other.imagePath != null)
-				return false;
-		} else if (!imagePath.equals(other.imagePath))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (playDate == null) {
-			if (other.playDate != null)
-				return false;
-		} else if (!playDate.equals(other.playDate))
-			return false;
-		if (synopsis == null) {
-			if (other.synopsis != null)
-				return false;
-		} else if (!synopsis.equals(other.synopsis))
-			return false;
-		if (viewingTime == null) {
-			if (other.viewingTime != null)
-				return false;
-		} else if (!viewingTime.equals(other.viewingTime))
-			return false;
-		return true;
+	public void setSynopsis(String synopsis) {
+		this.synopsis = synopsis;
+	}
+
+	public String getImagePath() {
+		return imagePath;
+	}
+
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
 	}
 
 	@Override
 	public String toString() {
-		return "Movie [name=" + name + ", imagePath=" + imagePath
-				+ ", director=" + director + ", actor=" + actor + ", synopsis="
-				+ synopsis + ", playDate=" + playDate + ", viewingTime="
-				+ viewingTime + "]";
+		return "Movie [genre=" + genre + ", name=" + name + ", director="
+				+ director + ", actor=" + actor + ", playDate=" + playDate
+				+ ", watchGrade=" + watchGrade + ", viewingTime=" + viewingTime
+				+ ", synopsis=" + synopsis + ", imagePath=" + imagePath + "]";
 	}
 
 }
